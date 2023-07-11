b2badc7de1d4dedd8e4ed0b8f5a3d73ced581a55
bossbasic <79305706+bossbasic@users.noreply.github.com>
2023-07-11 12:20:22 +0700

    dsad

--- Test.js
+++ Test.js
@@ -1,4 +1,12 @@
-var t = setInterval(function() {
-    console.log('test2')
-  }, 1000)
-  console.log('test1')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Thanapong')
+})
+
+app.listen(port, () => {
+  console.log(`on port ${port}`)
+  console.log('IT CHAIYAPHUM')
+})
